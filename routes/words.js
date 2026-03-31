385910df8300dbaf00be36099ca21acfc0f90e6c
gabe <gabrielz022604@gmail.com>
2026-03-31 09:59:21 -0400

    allwords routes and js

--- routes/words.js
+++ routes/words.js
@@ -13,6 +13,10 @@ router.get('/wotd', async (req,res)=>{
     res.render('wotd', {word:word, part:part, definition:definition});
 });
 
+router.get('/allwords', async (req,res)=>{
+    const allWords = await getAllWords();
+    res.render('allwords', {allwords:allWords});
+})
 
 let getWordFromDictionary = async ()=>{
     try{
@@ -25,6 +29,23 @@ let getWordFromDictionary = async ()=>{
     } catch (err){
         console.log("There was an error")
     }  
+};
+
+
+let getAllWords = async()=>{
+    try{
+        const data = await readFile('resources/allwords.txt', 'utf8');
+        let lines = data.split('\n');
+        let allwords = [];
+        for (line of lines){
+            let [word,part,defn] = line.split('\t');
+            let subObj = {word:word, part:part, defn:defn};
+            allwords.push(subObj);
+        }
+        return allwords;
+    } catch(err){
+        console.log("There was an error")
+    }
 };
 
 module.exports= router;
